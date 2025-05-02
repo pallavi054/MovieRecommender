1e0ef5ab5373fb69387efc157abbb09aae462736
Pallavi Aggarwal <102867195+pallavi054@users.noreply.github.com>
2025-05-01 19:12:59 -0500

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -13,19 +13,19 @@ using namespace std;
 
 class Movie {
     public:
-		// Functions go here..
-
-		// Default class constructor
-
-		// Parametrized class constructor
+    	// Functions go here..
+    
+    	// Default class constructor
+    
+    	// Parametrized class constructor
 
     private:
-		string title;     // Name of the movie
-		int year;	      // Release year
-		string genre;     // Movie category (Action, Comedy, Drama, etc.)
-		string director;  // Name of movie’s director
-		string cast;  	  // Name of the primary actor/actress appearing in the movie
-		int runtime;      // The runtime length of the movie, in minutes
-		vector<double> viewerRatings;  // Store each viewer’s ratings for the movie
+    	string title;     // Name of the movie
+    	int year;	      // Release year
+    	string genre;     // Movie category (Action, Comedy, Drama, etc.)
+    	string director;  // Name of movie’s director
+    	string cast;  	  // Name of the primary actor/actress appearing in the movie
+    	int runtime;      // The runtime length of the movie, in minutes
+    	vector<double> viewerRatings;  // Store each viewer’s ratings for the movie
 };
 #endif
