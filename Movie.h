08ebd82aa1ee390144b7668ed88ca0ffbfaed510
Pallavi Aggarwal <102867195+pallavi054@users.noreply.github.com>
2025-05-01 19:05:53 -0500

    Added bones of the class

--- Movie.h
+++ Movie.h
@@ -1,1 +1,26 @@
+/**
+ * Movie.h
+ * The Movie class defines ...
+ **/
 
+#ifndef MOVIE_H
+#define MOVIE_H
+
+class Movie {
+	public:
+	    // Functions go here..
+
+		// Default class constructor
+
+		// Parametrized class constructor
+	
+	private:
+	    string title;     // Name of the movie
+		int year;	      // Release year
+		string genre;     // Movie category (Action, Comedy, Drama, etc.)
+		string director;  // Name of movie’s director
+		string cast;  	  // Name of the primary actor/actress appearing in the movie
+		int runtime;      // The runtime length of the movie, in minutes
+		vector<double> viewerRatings;  // Store each viewer’s ratings for the movie
+};
+#endif
